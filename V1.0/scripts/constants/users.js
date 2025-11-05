d099145d5ddb5e4a409ced68acac3c702d0016e1
Mr. Syntax <maam98617@gmail.com>
2025-11-05 23:18:18 +0330

    Delete V1.0 directory

--- V1.0/scripts/constants/users.js
+++
@@ -1,5 +0,0 @@
-export const users = [{username : 'ali',password : 'ali'},
-    {username : 'reza',password : 'reza'}
-]
-
-export default users;
--- V1.0/scripts/utils/createLoginSession.js
+++
@@ -1,5 +0,0 @@
-export const createLoginSession = (username)=>{
-    localStorage.setItem('logged-in',JSON.stringify({username: username}))
-}
-
-export default createLoginSession;
